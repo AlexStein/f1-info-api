fce97b875b2d6714a849cbf866df9aec58a78507
Aleksandr Shevelev <alex.s@softmine.ru>
2021-04-07 09:28:08 +0300

    Initial commit

---
+++ app/src/main/java/ru/softmine/f1infoapi/di/AppComponent.kt
@@ -0,0 +1,14 @@
+package ru.softmine.f1infoapi.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+class AppComponent {
+    @Singleton
+    @Component(
+        modules = [
+
+        ]
+    )
+    interface AppComponent
+}
